b57bae193c2b100d05f5fc2164ba1ed4f0e10e20
Mohammed Mahi <mohammed.mahi012@gmail.com>
2024-03-26 21:14:03 +0000

    add: is work exist or not

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,31 +1,44 @@
 import TitlePage from "../components/TitlePage/TitlePage"
 import WorkRow from "../components/WorkRow/WorkRow"
 
+import { TfiFaceSad } from "react-icons/tfi";
 
 import vid from '../assets/videos/ecommerce/ecommerce.mp4'
-import { Element } from "react-scroll"
+import {Element} from "react-scroll"
 
-
-
-
+const datas = [
+    {vid:vid , title:"Ecommerce", label:"Web Site"},
+    {vid:vid , title:"Stock Gestion", label:"Web Site"},
+]
 const Works = () => {
-  return (
-    <Element name="Works" className="works">
-      
-      <div  className="container">
-      
-      <TitlePage title="Works"/>
-      
-      </div>
+    const handleWorks= false
 
+    return (
+        <Element name="Works" className="works">
 
-      <WorkRow vid={vid} title="Ecommerce" label="Web Site"/>
-      <WorkRow vid={vid} title="Stock Gestion" label="Web Site"/>
-      <WorkRow vid={vid} title="Ecommerce" label="Web Site"/>
-    
-    
-    </Element>
-  )
+            <div className="container">
+
+                <TitlePage title="Works"/>
+
+            </div>
+
+            {
+                handleWorks
+                    ?
+                    datas.map((e,index)=>{
+                        return(
+                            <WorkRow vid={e.vid} title={e.title} label={e.label} key={index}/>
+                        )
+                    })
+
+                    :
+                    <div className="closed-work">
+                        There are no works yet <TfiFaceSad/>
+                    </div>
+            }
+
+        </Element>
+    )
 }
 
 export default Works
